1402559f7b7c153541e5d50b5bb782c921e48708
Baja Stephanus <146203885+bajasiagian@users.noreply.github.com>
2024-04-25 14:26:18 +0000

    update

--- api.py
+++ api.py
@@ -37,8 +37,7 @@ def get_route_reccomendation(location, days):
     return prompt
 
 def get_chatgpt_ans(prompt):
-    client = OpenAI(organization=st.secrets['ORG'],
-                    project=st.secrets['proj'])
+    client = OpenAI(organization=st.secrets['ORG'])
 
     stream = client.chat.completions.create(
         model = "gpt-3.5-turbo",
